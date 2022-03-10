4530e79bf165c510239e371bf164dd6ce8bd60a2
D33064 <james0120160379@gmail.com>
2022-03-11 00:58:20 +0800

    .

--- SystemDesign/signup.php
+++ SystemDesign/signup.php
@@ -42,13 +42,8 @@
                             <label class="form-label" for="form3Example4cdg">Repeat your password</label>
                         </div>
         
-                        <div class="form-check d-flex justify-content-center mb-5">
-                            <input
-                            class="form-check-input me-2"
-                            type="checkbox"
-                            value=""
-                            id="form2Example3cg"
-                            />
+                        <div class="form-check d-flex  mb-5">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg"/>
                             <label class="form-check-label" for="form2Example3g">
                             I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
                             </label>
