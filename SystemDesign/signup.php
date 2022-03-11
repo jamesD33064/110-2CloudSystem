5580709c9854cd778658b53a12c1df1807703847
D33064 <james0120160379@gmail.com>
2022-03-11 19:00:38 +0800

    .

---
+++ SystemDesign/signup.php
@@ -0,0 +1,40 @@
+<?php
+  
+    $username="";
+    $password="";
+    $dbname="fcu";
+
+    $link = mysqli_connect("localhost" , $dbname , "A123456789" , $dbname) 
+            or die("fault");
+    echo "successful<br>";
+    
+    $username=$_POST["username"];
+    $email=$_POST["email"];
+    $password=$_POST["password"];
+
+    $sql = " SELECT * FROM `student`; ";
+    $i = 0;
+
+    if($result = mysqli_query($link,$sql)){
+        while($row = mysqli_fetch_assoc($result)){
+            if($row["id"]==$username && $row["password"]==$password){
+                echo "登入成功".$row["id"]."-".$row["password"]."<br>";
+                $i=1;
+                break;
+            }
+        }
+        if($i != 1){
+            echo "登入失敗";
+        }
+        mysqli_free_result($result);
+    }
+    mysqli_close($link);
+
+    
+    // $password_hash=password_hash($password,PASSWORD_DEFAULT);
+    
+
+
+
+?>
+
