30ab23982486c88b2948814e6edcf8e2d5053c64
D33064 <james0120160379@gmail.com>
2022-03-11 11:21:56 +0800

    .

--- SystemDesign/index.php
+++ SystemDesign/index.php
@@ -13,16 +13,111 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   </head>
   <body>
-    <header>
-      <iframe src="nvbar.html" width="100%" height="5%"></iframe>
-    </header>
+        <header>
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            
+                <a class="navbar-brand" href="index.html">
+                    <img src=" " width="30" height="30" class="d-inline-block align-top" alt="">
+                    <span class="h3 mx-1">系統分析與設計</span>
+                </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <!-- .collapse.navbar-collapse 用於外層中斷點群組和隱藏導覽列內容 -->
+                <!-- 選單項目&漢堡式折疊選單 -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <!-- active表示當前頁面 -->
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">首頁</a>   
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">關於我</a>
+                    </li>
+                    <!-- .dropdown Navbar選項使用下拉式選單 -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">分類</a>
+                        <!-- .dropdown-menu 下拉選單內容 -->
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">紙本書</a>
+                            <a class="dropdown-item" href="#">電子書</a>
+                        </div>
+                    </li>
+                </ul>
+                    
+                    <!-- <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form> -->
+                    
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="signin.php"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
+                    <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                </ul>
+                </div>
+                
+            </nav>
+        </header>
 
   <main class="my-5">
     <div class="container">
+
+      <div class="imgslidw">
+        <style>
+          .carousel-inner img {
+            width: 100%; /* Set width to 100% */
+            min-height: 200px;
+          }
+
+          /* Hide the carousel text when the screen is less than 600 pixels wide */
+          @media (max-width: 600px) {
+            .carousel-caption {
+              display: none; 
+            }
+          }
+        </style>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="https://placehold.it/800x400?text=IMAGE" alt="Image">
+              <div class="carousel-caption">
+                <h3>Sell $</h3>
+                <p>Money Money.</p>
+              </div>      
+            </div>
+
+            <div class="item">
+              <img src="https://placehold.it/800x400?text=Another Image Maybe" alt="Image">
+              <div class="carousel-caption">
+                <h3>More Sell $</h3>
+                <p>Lorem ipsum...</p>
+              </div>      
+            </div>
+          </div>
+
+          <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+
       <section class="text-center">
         <h4 class="mb-5"><strong>Latest posts</strong></h4>
-
-
         <div class="row">
           <div class="col-lg-4 col-md-12 mb-4">
             <div class="card">
