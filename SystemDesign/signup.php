06ac34bee3a3ca25b60fa7f2b0ac6851c74a1333
D33064 <james0120160379@gmail.com>
2022-03-12 02:18:56 +0800

    .

--- SystemDesign/signup.php
+++ SystemDesign/signup.php
@@ -18,14 +18,14 @@
                 VALUES('".$username."','".$password."')";
         if(mysqli_query($link, $sql)){
             echo "註冊成功!3秒後將自動跳轉頁面<br>";
-            echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
+            echo "<a href='index.php'>未成功跳轉頁面請點擊此</a>";
             // header("refresh:32;url=index.php");
             exit;
         }
     }
     else{
         echo "該帳號已有人使用!<br>3秒後將自動跳轉頁面<br>";
-        echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
+        echo "<a href='index.php'>未成功跳轉頁面請點擊此</a>";
         // header('HTTP/1.0 302 Found');
         //header("refresh:3;url=register.html",true);
         exit;
